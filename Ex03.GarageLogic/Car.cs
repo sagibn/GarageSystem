4b89ad2ae31cb3ae15f12848462a6fb777774649
ofir cohen <ofircohen110@gmail.com>
2023-05-20 15:05:33 +0300

    Added car + truck + grage (not coded yet) + car details

---
+++ Ex03.GarageLogic/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+	public enum eCarColor
+	{ 
+		White,
+		Black,
+		Yellow,
+		Red
+	}
+	public class Car : Vehicle
+	{
+		int m_NumOfDoors;
+		eCarColor m_ColorOfCar;
+
+		public Car(eCarColor i_CarColor, int i_NumOfDoors, string i_ManufactureName, float i_CurrAirPressure, string i_ModelName,
+			string i_LicenceNumber, EnergySource i_Engine)
+			: base(i_ModelName, i_LicenceNumber, i_Engine)
+		{
+			SetWheels(5, i_ManufactureName, i_CurrAirPressure, 33.0f);
+			m_ColorOfCar = i_CarColor;
+			m_NumOfDoors = i_NumOfDoors;
+		}
+
+		public eCarColor CarColor
+		{
+			get
+			{
+				return m_ColorOfCar;
+			}
+		}
+
+		public int NumOfDoors
+		{
+			get
+			{
+				return m_NumOfDoors;
+			}
+		}
+
+		public override string GetVehicleData()
+		{
+			string carData = string.Format(@"Car:
+{0}
+{1}
+Number of doors: {2}
+Color of the car: {3}", base.GetVehicleData(), Engine.ToString(), m_NumOfDoors, m_ColorOfCar);
+
+			return carData;
+		}
+	}
+}
---
+++ Ex03.GarageLogic/Truck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ex03.GarageLogic
+{
+
+	public class Truck : Vehicle
+	{
+		bool m_HazardElements;
+		float m_CargoVolume;
+
+		public Car(string i_ManufactureName, float i_CurrAirPressure, 
+			string i_ModelName, string i_LicenceNumber, EnergySource i_Engine, 
+			float i_CargoVolume, bool i_HazardElements) : base(i_ModelName, i_LicenceNumber, i_Engine)
+		{
+			SetWheels(14, i_ManufactureName, i_CurrAirPressure, 26.0f);
+			m_CargoVolume = i_CargoVolume;
+			m_HazardElements = i_HazardElements;
+		}
+
+		public bool HazardElements
+		{
+			get
+			{
+				return m_HazardElements;
+			}
+		}
+
+		public bool CargoVolume
+		{
+			get
+			{
+				return m_CargoVolume;
+			}
+		}
+
+		public override string GetVehicleData()
+		{
+			string truckData = string.Format(@"Truck:
+{0}
+{1}
+Contains hazard elements: {2}
+Cargo volume: {3}", base.GetVehicleData(), Engine.ToString(), m_HazardElements, m_CargoVolume);
+
+			return truckData;
+		}
+	}
+}
