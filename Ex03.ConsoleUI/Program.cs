6ea335a7a958999c1528bd84c25f30c21899567f
ofir cohen <ofircohen110@gmail.com>
2023-05-23 21:09:23 +0300

    added ui

---
+++ Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex03.ConsoleUI
+{
+    class Program
+    {
+        public static void Main()
+        {
+            UserInterface UI = new UserInterface();
+
+            UI.Run();
+        }
+    }
+}
